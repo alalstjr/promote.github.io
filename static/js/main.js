3fcbbc73b520e1192602809f5cb569410bbf39a5
alalstjr <42495072+alalstjr@users.noreply.github.com>
2018-08-21 01:22:36 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,12 @@
 	$(document).ready(function(){
 		$('.menu-ul .menu-li').on('click',function(){
 			var index = $(this).index();
-			$('.menu-ul .sub-ul').removeClass('active');
-			$('.menu-ul .sub-ul').eq(index).addClass('active');
 			if( $(this).find('.sub-ul').hasClass('active') ){
 				$('.menu-ul .sub-ul').removeClass('active');
-			};
+			} else {
+				$('.menu-ul .sub-ul').removeClass('active');
+				$('.menu-ul .sub-ul').eq(index).addClass('active');
+			}	
 		});
 	});
 })(jQuery);
